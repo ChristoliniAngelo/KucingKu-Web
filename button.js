4333ea083fa42a93ddb1d1aa7c0dbbe3eb5776f4
ChristoliniAngelo <christolini.angelo.20@student.ce.ittelkom-sby.ac.id>
2024-03-07 12:53:06 +0700

    clean the code

--- button.js
+++ button.js
@@ -1,19 +1,15 @@
-// Mendapatkan referensi tombol "Mulai!"
 var startButton = document.querySelector('.content-button');
 
-// Mendefinisikan fungsi untuk memainkan suara
 function playSound() {
-    var audio = new Audio('properties/suara/shari_meow-47485.mp3'); // Perbarui path audio
+    var audio = new Audio('properties/suara/shari_meow-47485.mp3'); // path audio
     audio.play();
 
-    // Memberhentikan pemutaran suara setelah 2 detik
     setTimeout(function() {
-        audio.pause(); // Menghentikan pemutaran suara
-        audio.currentTime = 0; // Mengembalikan ke awal audio
-    }, 2000); // 2000 milidetik = 2 detik
+        audio.pause(); 
+        audio.currentTime = 0; 
+    }, 2000);
 }
 
-// Menambahkan event listener untuk mendeteksi klik pada tombol "Mulai!"
 startButton.addEventListener('click', function() {
     playSound();
 });
