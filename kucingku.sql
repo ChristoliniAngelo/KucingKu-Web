c9d0040b1df208666bb9231da332507f87d22fc7
ChristoliniAngelo <christolini.angelo.20@student.ce.ittelkom-sby.ac.id>
2024-06-18 14:37:55 +0700

    update db

--- kucingku.sql
+++ kucingku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 09:29 AM
+-- Generation Time: Jun 18, 2024 at 09:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,7 +48,7 @@ INSERT INTO `cats` (`id`, `nama_kucing`, `jenis_kelamin`, `umur`, `warna`, `loka
 (2, 'Bedok', 'Betina', 'Baby', 'Coklat', 'Surabaya', 'Belum', 'assets/cat_image/Bedok.png', 0),
 (3, 'Januari', 'Jantan', 'Baby', 'Coklat', 'Surabaya', 'Sudah', 'assets/cat_image/Januari.png\r\n', 4),
 (4, 'Iwak', 'Jantan', 'Baby', 'Abu-Abu', 'Surabaya', 'Belum', 'assets/cat_image/Iwak.png', 25),
-(5, ' Moza', 'Betina', 'Adult', 'Abu-Abu', 'Surabaya', 'Belum', 'assets/cat_image/Moza.png\r\n', 11),
+(5, 'Moza', 'Betina', 'Adult', 'Abu-Abu', 'Surabaya', 'Belum', 'assets/cat_image/Moza.png\r\n', 11),
 (6, 'Theo', 'Jantan', 'Young', 'Orange', 'Surabaya', 'Belum', 'assets/cat_image/Theo.png', 24),
 (7, 'Chika', 'Betina', 'Adult', 'Orange', 'Surabaya', 'Sudah', 'assets/cat_image/Chika_1.png', 22),
 (8, 'Mao', 'Betina', 'Baby', 'Abu-Abu', 'Surabaya', 'Sudah', 'assets/cat_image/Mao.png', 19),
@@ -97,13 +97,13 @@ INSERT INTO `cats` (`id`, `nama_kucing`, `jenis_kelamin`, `umur`, `warna`, `loka
 (51, 'Ulir', 'Betina', 'Baby', 'Coklat', 'Surabaya', 'Sudah', 'assets/cat_image/Ulir.png', 26),
 (52, 'Coco', 'Jantan', 'Adult', 'Putih', 'Sidoarjo', 'Sudah', 'assets/cat_image/Coco.png', 1),
 (53, 'Milo', 'Jantan', 'Young', 'Hitam', 'Kota Lainnya', 'Belum', 'assets/cat_image/Milo_2.png', 9),
-(54, 'Unyil', 'Jantan', 'Adult', 'Orange', 'Surabaya', 'Sudah', 'assets/cat_image/Unyit.png', 18),
+(54, 'Unyil', 'Jantan', 'Adult', 'Orange', 'Surabaya', 'Sudah', 'assets/cat_image/Unyil.png', 18),
 (55, 'Fino', 'Jantan', 'Adult', 'Coklat', 'Kota Lainnya', 'Belum', 'assets/cat_image/Fino.png', 17),
 (56, 'Kitty', 'Betina', 'Adult', 'Abu-Abu', 'Kota Lainnya', 'Sudah', 'assets/cat_image/Kitty.png', 11),
 (57, 'Grimjoew', 'Jantan', 'Young', 'Orange', 'Sidoarjo', 'Sudah', 'assets/cat_image/Grimjoew.png', 27),
 (58, 'Winnie', 'Betina', 'Adult', 'Abu-Abu', 'Kota Lainnya', 'Sudah', 'assets/cat_image/Winnie.png', 12),
-(59, 'Trisie Beiji', 'Betina', 'Adult', 'Hitam', 'Jakarta', 'Sudah', 'assets/cat_image/Trisie Beiji.png', 21),
-(60, 'Trisi Sukmajaya', 'Betina', 'Young', 'Hitam', 'Jakarta', 'Sudah', 'assets/cat_image/Trisi Sukmajata.png', 0),
+(59, 'Trisie Beiji', 'Betina', 'Adult', 'Hitam', 'Jakarta', 'Sudah', 'assets/cat_image/Trisie_Beiji.png', 21),
+(60, 'Trisi Sukmajaya', 'Betina', 'Young', 'Hitam', 'Jakarta', 'Sudah', 'assets/cat_image/Trisi_Sukmajata.png', 0),
 (61, 'Uli', 'Betina', 'Young', 'Putih', 'Jakarta', 'Sudah', 'assets/cat_image/Uli.png', 5),
 (62, 'Chika', 'Betina', 'Adult', 'Putih', 'Jakarta', 'Sudah', 'assets/cat_image/Chika.png', 3),
 (63, 'Cookies', 'Betina', 'Adult', 'Hitam', 'Jakarta', 'Sudah', 'assets/cat_image/Cookies.png', 21),
@@ -161,62 +161,20 @@ CREATE TABLE `cat_preference` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `cat_preference`
+-- Table structure for table `rating`
 --
 
-INSERT INTO `cat_preference` (`id`, `user_id`, `jenis_kelamin`, `usia`, `warna`, `vaksinasi`, `created_at`) VALUES
-(1, 1, 'Jantan', 'Baby', 'Hitam', 'Sudah', '2024-04-23 14:51:51'),
-(2, 2, 'Betina', 'Young', 'Putih', 'Sudah', '2024-04-23 16:51:57'),
-(3, 2, 'Jantan', 'Young', 'Putih', 'Sudah', '2024-04-27 14:46:57'),
-(4, 3, 'Jantan', 'Young', 'Putih', 'Sudah', '2024-05-06 17:43:19'),
-(5, 4, 'Betina', 'Young', 'Hitam', 'Sudah', '2024-05-10 01:50:05'),
-(6, 4, 'Jantan', 'Baby', 'Hitam', 'Sudah', '2024-05-17 08:44:29'),
-(7, 4, 'Jantan', 'Baby', 'Coklat', 'Sudah', '2024-05-31 06:38:28'),
-(8, 4, 'Jantan', 'Baby', 'Coklat', 'Sudah', '2024-05-31 07:00:43'),
-(9, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-05-31 07:02:16'),
-(10, 4, 'Betina', 'Young', 'Putih', 'Sudah', '2024-05-31 07:10:55'),
-(11, 4, 'Betina', 'Young', 'Hitam', 'Sudah', '2024-05-31 07:18:24'),
-(12, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-05-31 07:59:29'),
-(13, 4, 'Jantan', 'Baby', 'Coklat', 'Sudah', '2024-05-31 08:01:21'),
-(14, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-05-31 08:14:38'),
-(15, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-05-31 08:26:53'),
-(16, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-05-31 08:41:22'),
-(17, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-06-03 04:55:33'),
-(18, 4, 'Jantan', 'Baby', 'Coklat', 'Belum', '2024-06-03 05:00:20'),
-(19, 4, 'Jantan', 'Baby', 'Abu-Abu', 'Belum', '2024-06-03 05:06:05'),
-(20, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-03 05:07:47'),
-(21, 4, 'Jantan', 'Young', 'Abu-Abu', 'Belum', '2024-06-03 05:28:28'),
-(22, 4, 'Jantan', 'Young', 'Orange', 'Belum', '2024-06-03 05:31:31'),
-(23, 4, 'Jantan', 'Baby', 'Abu-Abu', 'Belum', '2024-06-03 05:34:40'),
-(24, 4, 'Jantan', 'Young', 'Abu-Abu', 'Belum', '2024-06-03 06:35:41'),
-(25, 4, 'Jantan', 'Baby', 'Orange', 'Sudah', '2024-06-03 06:39:51'),
-(26, 4, 'Betina', 'Baby', 'Abu-Abu', 'Belum', '2024-06-03 06:52:24'),
-(27, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-03 09:12:07'),
-(28, 4, 'Betina', 'Adult', 'Hitam', 'Sudah', '2024-06-03 09:14:26'),
-(29, 4, 'Jantan', 'Baby', 'Abu-Abu', 'Belum', '2024-06-03 09:16:04'),
-(30, 4, 'Jantan', 'Young', 'Putih', 'Sudah', '2024-06-03 09:17:06'),
-(31, 4, 'Betina', 'Baby', 'Putih', 'Sudah', '2024-06-03 09:18:49'),
-(32, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-03 09:20:29'),
-(33, 4, 'Jantan', 'Young', 'Abu-Abu', 'Belum', '2024-06-03 09:26:56'),
-(34, 4, 'Jantan', 'Young', 'Abu-Abu', 'Belum', '2024-06-03 09:28:15'),
-(35, 4, 'Jantan', 'Adult', 'Putih', 'Sudah', '2024-06-04 03:28:21'),
-(36, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-05 04:41:27'),
-(37, 4, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-06-05 07:49:39'),
-(38, 4, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-08 02:22:07'),
-(39, 4, 'Betina', 'Baby', 'Putih', 'Belum', '2024-06-08 04:08:47'),
-(40, 4, 'Jantan', 'Baby', 'Abu-Abu', 'Belum', '2024-06-08 04:11:33'),
-(41, 4, 'Betina', 'Baby', 'Coklat', 'Sudah', '2024-06-08 04:22:18'),
-(42, 4, 'Jantan', 'Young', 'Putih', 'Sudah', '2024-06-08 04:39:45'),
-(43, 5, 'Jantan', 'Young', 'Coklat', 'Sudah', '2024-06-08 04:41:53'),
-(44, 5, 'Betina', 'Baby', 'Abu-Abu', 'Sudah', '2024-06-10 03:53:56'),
-(45, 5, 'Jantan', 'Baby', 'Putih', 'Sudah', '2024-06-10 03:55:35'),
-(46, 4, 'Betina', 'Young', 'Coklat', 'Sudah', '2024-06-11 06:16:38'),
-(47, 5, 'Jantan', 'Adult', 'Putih', 'Sudah', '2024-06-11 06:21:32'),
-(48, 5, 'Betina', 'Baby', 'Putih', 'Sudah', '2024-06-11 06:32:37'),
-(49, 5, 'Jantan', 'Baby', 'Coklat', 'Sudah', '2024-06-11 06:55:50'),
-(50, 5, 'Jantan', 'Baby', 'Abu-Abu', 'Belum', '2024-06-11 07:21:43'),
-(51, 5, 'Betina', 'Young', 'Hitam', 'Belum', '2024-06-11 07:23:46');
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `cat_id` int(11) DEFAULT NULL,
+  `ClusterKucing` varchar(255) DEFAULT NULL,
+  `Rating` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -240,11 +198,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `PASSWORD`, `created_at`, `userage`, `usergender`, `userlocation`) VALUES
-(1, 'Elo', 'Christoliniangelo@gmail.com', '$2b$10$sWcJLsvMtbK2F.GhlmROYeiO6aD4PjwN1mMrIAnLGbIhtfZfzm6Aa', '2024-04-23 14:13:32', 21, 'Male', 'Surabaya'),
-(2, 'admin', 'admin@gmail.com', '$2b$10$xZwGf4bRdQC/fYhjwb8GQeVNFZguo5ZzW7i.ZrQEGP7NFc0ufJMY2', '2024-04-23 16:51:38', 24, 'Female', 'Jakarta'),
-(3, 'rama', 'rama@gmail.com', '$2b$10$t0FCeXGKnXzNiYQJAEhzEu7tuIxj0b4FLMMrZguxhf5TlCTTbRtb2', '2024-05-06 17:43:03', 23, 'Male', 'Surabaya'),
-(4, 'test', 'admin1@gmail.com', '$2b$10$uVG.airBgw42TZMII5GbcekoI6vwxp4Vc2iWxCbBy6Xm44ewlLoUO', '2024-05-09 19:51:57', 30, 'Male', 'Jakarta'),
-(5, 'jen', 'jenjelek@jelek.com', '$2b$10$GAVsUNKT5VlTzorjVqfWT.WIk.BVLO97hPSj9iGMHZRtWoGn56G6C', '2024-06-08 04:41:40', 28, 'Female', 'Surabaya');
+(0, 'Testing', 'Test@email.com', '$2b$10$gceHlPtol4huskaMj9lx8Ojd7BEQCKrUbwh2FZwXa7vFDU.fYR3cK', '2024-06-12 02:26:21', 22, 'Male', 'Surabaya');
 
 --
 -- Indexes for dumped tables
@@ -262,6 +216,14 @@ ALTER TABLE `cats`
 ALTER TABLE `cat_preference`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `cat_id` (`cat_id`);
 
 --
 -- Indexes for table `user`
@@ -283,7 +245,13 @@ ALTER TABLE `cats`
 -- AUTO_INCREMENT for table `cat_preference`
 --
 ALTER TABLE `cat_preference`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -301,6 +269,13 @@ ALTER TABLE `user`
 ALTER TABLE `cat_preference`
   ADD CONSTRAINT `cat_preference_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `cats` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
